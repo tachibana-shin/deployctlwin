0a14f82e755b26a58757efc34643484814bc6dad
Tachibana Shin <shin@shin.is-a.dev>
2025-04-05 00:40:57 +0700

    Remove check permission file store token

--- src/utils/token_storage/fs.ts
+++ src/utils/token_storage/fs.ts
@@ -3,23 +3,12 @@ import { getConfigPaths } from "../info.ts";
 export async function get(): Promise<string | null> {
   const { credentialsPath } = getConfigPaths();
   try {
-    const info = await Deno.lstat(credentialsPath);
-    if (!info.isFile || (info.mode !== null && (info.mode & 0o777) !== 0o600)) {
-      throw new Error(
-        "The credentials file has been tampered with and will be ignored. Please delete it.",
-      );
-    }
-  } catch (e) {
-    if (e instanceof Deno.errors.NotFound) {
-      return null;
-    } else {
-      throw e;
-    }
-  }
-  try {
     const token = JSON.parse(await Deno.readTextFile(credentialsPath)).token;
     return token || null;
   } catch (_) {
+    if (e instanceof Deno.errors.NotFound) {
+      return null;
+    }
     throw new Error(
       `The credentials file has been tampered with and will be ignored. Please delete it.`,
     );
